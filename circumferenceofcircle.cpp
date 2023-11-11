1c175fa9e0d52fc1f1b3a6d3ad77e59d85634290
Veera Pramod Addu <107345227+Veerapramodaddu@users.noreply.github.com>
2023-11-11 23:32:37 +0530

    solved assignment files are added

---
+++ circumferenceofcircle.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float radius=10;
+    float circumference;
+    circumference=2*3.1415*radius;
+    cout<<circumference;
+    return 0;
+    
+
+}
